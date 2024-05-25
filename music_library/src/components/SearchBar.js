f5deee09e6f04009d1dc1d2ff82073cc010e8c45
Andi Weber <adtweber@gmail.com>
2024-05-25 12:47:55 -0400

    built out artist and album view components

--- music_library/src/components/SearchBar.js
+++ music_library/src/components/SearchBar.js
@@ -1,19 +1,14 @@
 import { useState } from 'react'
 
-function SeachBar(props) {
+const SearchBar = (props) => {
     let [searchTerm, setSearchTerm] = useState('')
 
     return (
         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
-            <input type="text" placeholder="Enter a search term here" onChange={
-                (e) => setSearchTerm(e.target.value)
-            }/>
-
+            <input type="text" placeholder="Search Here" onChange={(e) => setSearchTerm(e.target.value)} />
             <input type="submit" />
-
         </form>
     )
 }
 
-export default SeachBar
+export default SearchBar;
